fb331e25ae68339262eb88f2fc002ed49ee77f01
Pablo Bousquets-Muñoz <bousquetspablo@uniovi.es>
2022-11-08 17:36:34 +0000

    FlipFlopModel implementation (not ready yet)

---
+++ src/dr/evomodel/substmodel/FlipFlopModel.java
@@ -0,0 +1,184 @@
+/*
+ * FlipFlopModel.java
+ *
+ * Copyright (c) 2002-2015 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodel.substmodel;
+
+import dr.evolution.datatype.AFsequence;
+import dr.inference.model.Variable;
+import dr.inference.model.Parameter;
+import dr.inference.model.Model;
+
+
+/**
+ * @author Chieh-Hsi Wu
+ *
+ * A class for the flipflop model
+ */
+
+public class FlipFlopModel extends ComplexSubstitutionModel{
+
+    /**
+     * Constructor
+     * @param name              Model name
+     * @param stemCellParam     Number of stem cells
+     * @param gammaParam        Gamma parameter
+     * @param lambdaParam       Lambda parameter
+     * @param muParam           Mu parameter
+     * @param freqModel         Frequency model
+     */
+
+    protected boolean updateMatrix = true;
+    private Parameter stemCellParam = null;
+    private Variable<Double> gammaParam = null;
+    private Variable<Double> lambdaParam = null;
+    private Variable<Double> muParam = null;
+    private int[][] stateVar;
+
+    public FlipFlopModel(String name, Parameter stemCellParam, Variable gammaParam, Variable lambdaParam, Variable muParam, FrequencyModel freqModel) {
+        super(name, freqModel.getDataType(), freqModel, null); //TODO: check the last parameter
+
+        // None of the variables should be null with the current parser, so no need to evaluate them, right?
+        addVariable(stemCellParam);
+        this.stemCellParam = stemCellParam;
+        addVariable(gammaParam);
+        this.gammaParam = gammaParam;
+        addVariable(lambdaParam);
+        this.lambdaParam = lambdaParam;
+        addVariable(muParam);
+        this.muParam = muParam;
+        generateStateVar();
+
+        updateMatrix = true;
+    }
+
+    protected void handleModelChangedEvent(Model model, Object object, int index) {
+        updateMatrix = true;
+    }
+
+    //store rate matrix
+    @Override
+    public void storeIntoAmat(){
+        int S = (int) stemCellParam.getParameterValue(0);
+        double gamma = gammaParam.getValue(0);
+        double lam = lambdaParam.getValue(0);
+        double mu = muParam.getValue(0);
+        int num_states = stateCount;
+
+        for (int down = 0; down < num_states; down++){ //Represents the new state
+            int k_down = stateVar[down][0];
+            int m_down = stateVar[down][1];
+            for (int across = 0; across < num_states; across++) { //Represents the old state
+                int k = stateVar[across][0];
+                int m = stateVar[across][1];
+                if (k == k_down-1 & m == m_down){
+                    amat[across][down] = (S-m-k) * (k*lam/(S-1) + 2*mu);
+                } else if (k == k_down & m == m_down-1){
+                    amat[across][down] = m * (S-m-k) * lam / (S-1);
+                } else if (k == k_down+1 & m == m_down-1) {
+                    amat[across][down] = k * (m*lam/(S-1)+mu);
+                } else if (k == k_down+1 & m == m_down) {
+                    amat[across][down] = k * ((S-m-k)*lam/(S-1)+gamma);
+                } else if (k == k_down & m == m_down+1) {
+                    amat[across][down] = m * (S-m-k) * lam / (S-1);
+                } else if (k == k_down-1 & m == m_down+1) {
+                    amat[across][down] = m * (k*lam/(S-1)+2*gamma);
+                } else if (k == k_down & m == m_down) {
+                    amat[across][down] = -(2*((k+m)*(S-m-k)+k*m)*lam/(S-1) + (k+2*m)*gamma + (2*S-(k+2*m))*mu);
+                } else {
+                    amat[across][down] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * This method will create a matrix with all possible cell states, for parameters k (one met allele) and m (both met alleles)
+     * For S=2, possible states are for (k,m): 0,0 0,1 0,2 1,0, 2,0 1,1
+     **/
+    public void generateStateVar(){
+        int S = (int) stemCellParam.getParameterValue(0);
+        int num_states = stateCount; //TODO: make sure this is working fine
+
+        int[][] stateVar = new int[num_states][];
+        int ii = 0;
+        for (int m = 0; m <= S; m++){
+            for (int k = 0; k <= S; k++){
+                if (k+m <=S) {
+                    this.stateVar[ii] = new int[]{k, m};
+                    ii++;
+                }
+            }
+        }
+        this.stateVar = stateVar;
+    }
+
+    //matrix is already valid
+    protected void makeValid(double[][] matrix, int dimension){}
+
+    /*
+     * Set up stationary frequencies
+     */
+    public void computeTwoPhaseStationaryDistribution(){
+        synchronized (this) {
+            if (updateMatrix) {
+                setupMatrix();
+            }
+        }
+
+        if (!wellConditioned) {
+            throw new RuntimeException("not well conditioned");
+        }
+        int eigenValPos = -1;
+
+        for(int i = 0; i < stateCount; i++){
+            if(Eval[i] == 0){
+                eigenValPos = i;
+                break;
+            }
+        }
+        /*for(int i = 0; i < EvalImag.length; i++){
+            //System.out.println("imaginery part" + EvalImag[i]);
+            if(EvalImag[i] != 0.0){
+                throw new RuntimeException("imaginery part" + EvalImag[i]);
+            }
+        }*/
+
+        double[] empFreq = new double[stateCount];
+        //System.out.println("eq dist");
+        for(int i = 0; i < stateCount; i++){
+            empFreq[i] = Evec[i][eigenValPos]*Ievc[eigenValPos][i];
+            //System.out.println(empFreq[i]);
+
+        }
+        this.freqModel = new FrequencyModel(dataType, empFreq);
+    }
+
+    public void setupMatrix(){
+        //setupInfinitesimalRates();
+        //super.setupMatrix();
+    }
+
+
+}
---
+++ src/dr/evomodelxml/substmodel/FlipFlopModelParser.java
@@ -0,0 +1,109 @@
+/*
+ * FlipFlopModelParser.java
+ *
+ * By DM, modified from HKYParser
+ *
+ * Copyright (c) 2002-2015 Alexei Drummond, Andrew Rambaut and Marc Suchard
+ *
+ * This file is part of BEAST.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * BEAST is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  BEAST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BEAST; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package dr.evomodelxml.substmodel;
+
+import dr.evolution.datatype.DataType;
+import dr.evolution.datatype.TwoStates;
+import dr.inference.model.Variable;
+import dr.inference.model.Parameter;
+import dr.evolution.datatype.AFsequence;
+import dr.evomodel.substmodel.FlipFlopModel;
+import dr.evomodel.substmodel.FrequencyModel;
+import dr.xml.*;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Alexei Drummond
+ * @author Andrew Rambaut
+ * @author Diego Mallo
+ */
+public class FlipFlopModelParser extends AbstractXMLObjectParser {
+
+    public static final String FLIPFLOP_MODEL = "flipflopModel";
+    public static final String STEM_CELLS = "stemCells";
+    public static final String GAMMA = "gamma";
+    public static final String LAMBDA= "lambda";
+    public static final String MU= "mu";
+
+    public String getParserName() {
+        return FLIPFLOP_MODEL;
+    }
+
+    public Object parseXMLObject(XMLObject xo) throws XMLParseException {
+
+        Parameter stemCellParam = (Parameter) xo.getElementFirstChild(STEM_CELLS);
+        Variable gammaParam = (Variable) xo.getElementFirstChild(GAMMA);
+        Variable lambdaParam = (Variable) xo.getElementFirstChild(LAMBDA);
+        Variable muParam = (Variable) xo.getElementFirstChild(MU);
+
+        int S = (int) stemCellParam.getParameterValue(0);
+        int stateCount = (int) 0.5*(S+1)*(S+2);
+        double[] freqs = new double[stateCount];
+        for(int i = 0; i < freqs.length; i++){
+            freqs[i] = 1.0/stateCount;
+        }
+
+        AFsequence afseq = new AFsequence(stateCount);
+        FrequencyModel freqModel = new FrequencyModel(AFsequence.INSTANCE, freqs);
+
+        Logger.getLogger("dr.evomodel").info("Creating FlipFlip model");
+        Logger.getLogger("dr.evomodel").info("  - Initial gamma:" + gammaParam.getValue(0));
+        Logger.getLogger("dr.evomodel").info("  - Initial lambda:" + lambdaParam.getValue(0));
+        Logger.getLogger("dr.evomodel").info("  - Initial mu:" + muParam.getValue(0));
+
+        return new FlipFlopModel(xo.getId(), stemCellParam, gammaParam, lambdaParam, muParam, freqModel);
+    }
+
+    //************************************************************************
+    // AbstractXMLObjectParser implementation
+    //************************************************************************
+
+    public String getParserDescription() {
+        return "This element represents an instance of the flipflop model (Gabbutt, 2022), depicting the dynamics of fluctuating CpG sites.";
+    }
+
+    public Class getReturnType() {
+        return FlipFlopModel.class;
+    }
+
+    public XMLSyntaxRule[] getSyntaxRules() {
+        return rules;
+    }
+
+    private final XMLSyntaxRule[] rules = {
+            new ElementRule(GAMMA,
+                    new XMLSyntaxRule[]{new ElementRule(Variable.class)}),
+            new ElementRule(LAMBDA,
+                    new XMLSyntaxRule[]{new ElementRule(Variable.class)}),
+            new ElementRule(MU,
+                    new XMLSyntaxRule[]{new ElementRule(Variable.class)}),
+            new ElementRule(STEM_CELLS,
+                    new XMLSyntaxRule[]{new ElementRule(Variable.class)})
+    };
+}
