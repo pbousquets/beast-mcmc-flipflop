e01e17e2d511ab8e54bce02bc48340d310284164
Pablo Bousquets-Muñoz <bousquetspablo@uniovi.es>
2023-04-16 10:12:38 +0200

    Added new options to choose random parameters

--- simulate/flipflopphylogenysimulate.py
+++ simulate/flipflopphylogenysimulate.py
@@ -9,15 +9,17 @@ import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 import os
 import json
+import random
 
 import msprime
 import re
 
+
 ## Custom click to allow grouping arguments
 class CustomOption(click.Option):
     def __init__(self, *args, **kwargs):
-        kwargs.update({'show_default': True})
-        self.help_group = kwargs.pop('help_group', None)
+        kwargs.update({"show_default": True})
+        self.help_group = kwargs.pop("help_group", None)
         super(CustomOption, self).__init__(*args, **kwargs)
 
 
@@ -28,14 +30,20 @@ class CustomCommand(click.Command):
         for param in self.get_params(ctx):
             rv = param.get_help_record(ctx)
             if rv is not None:
-                if hasattr(param, 'help_group') and param.help_group:
+                if hasattr(param, "help_group") and param.help_group:
                     opts.setdefault(str(param.help_group), []).append(rv)
                 else:
-                    opts.setdefault('Options', []).append(rv)
+                    opts.setdefault("Options", []).append(rv)
 
         for name, opts_group in opts.items():
             with formatter.section(name):
                 formatter.write_dl(opts_group)
+
+
+def get_random_seed():
+    r = os.urandom(4)
+    return int.from_bytes(r, byteorder="big")
+
 
 # Random tree
 def generate_random_tree(k, N, t, root_height, seed):
@@ -63,8 +71,10 @@ def generate_random_tree(k, N, t, root_height, seed):
     # Print the modified Newick string
     return newick
 
+
 def generate_state_var(S):
-    return [(k, m) for m in range(S+1) for k in range(S+1) if k+m <=S]
+    return [(k, m) for m in range(S + 1) for k in range(S + 1) if k + m <= S]
+
 
 def generate_rate_matrix(S, lam, mu, gamma, stateVar=None):
     if stateVar is None:
@@ -74,23 +84,23 @@ def generate_rate_matrix(S, lam, mu, gamma, stateVar=None):
 
     for down, (k_down, m_down) in enumerate(stateVar):
         for across, (k, m) in enumerate(stateVar):
-            if k == k_down-1 and m == m_down :
-                RateMatrix[down, across] = (S-m-k) * (k*lam/(S-1) + 2*mu)
-            elif k == k_down and m == m_down-1:
-                RateMatrix[down, across] = m * (S-m-k) * lam / (S-1)
-            elif k == k_down+1 and m == m_down-1:
-                RateMatrix[down, across] = k * (m*lam/(S-1)+mu)
-            elif k == k_down+1 and m == m_down:
-                RateMatrix[down, across] = k * ((S-m-k)*lam/(S-1)+gamma)
-            elif k == k_down and m == m_down+1:
-                RateMatrix[down, across] = m * (S-m-k) * lam / (S-1)
-            elif k == k_down-1 and m == m_down+1:
-                RateMatrix[down, across] = m * (k*lam/(S-1)+2*gamma)
+            if k == k_down - 1 and m == m_down:
+                RateMatrix[down, across] = (S - m - k) * (k * lam / (S - 1) + 2 * mu)
+            elif k == k_down and m == m_down - 1:
+                RateMatrix[down, across] = m * (S - m - k) * lam / (S - 1)
+            elif k == k_down + 1 and m == m_down - 1:
+                RateMatrix[down, across] = k * (m * lam / (S - 1) + mu)
+            elif k == k_down + 1 and m == m_down:
+                RateMatrix[down, across] = k * ((S - m - k) * lam / (S - 1) + gamma)
+            elif k == k_down and m == m_down + 1:
+                RateMatrix[down, across] = m * (S - m - k) * lam / (S - 1)
+            elif k == k_down - 1 and m == m_down + 1:
+                RateMatrix[down, across] = m * (k * lam / (S - 1) + 2 * gamma)
             elif k == k_down and m == m_down:
-                RateMatrix[down, across] = -(2*((k+m)*(S-m-k)+k*m)*lam/(S-1) +
-                                            (k+2*m)*gamma + (2*S-(k+2*m))*mu)
+                RateMatrix[down, across] = -(2 * ((k + m) * (S - m - k) + k * m) * lam / (S - 1) + (k + 2 * m) * gamma + (2 * S - (k + 2 * m)) * mu)
 
     return RateMatrix
+
 
 def multinomial_rvs(counts, p, rng=None):
     if rng is None:
@@ -101,11 +111,11 @@ def multinomial_rvs(counts, p, rng=None):
     out = np.zeros(np.shape(p), dtype=int)
     ps = np.cumsum(p[::-1, ...], axis=0)[::-1, ...]
     # Conditional probabilities
-    with np.errstate(divide='ignore', invalid='ignore'):
+    with np.errstate(divide="ignore", invalid="ignore"):
         condp = p / ps
     condp[np.isnan(condp)] = 0.0
 
-    for i in range(p.shape[0]-1):
+    for i in range(p.shape[0] - 1):
         binsample = rng.binomial(counts, condp[i, ...])
         out[i, ...] = binsample
         counts -= binsample
@@ -114,7 +124,8 @@ def multinomial_rvs(counts, p, rng=None):
 
     return out
 
-def evolve_state(RateMatrix, state, dt, rng = None):
+
+def evolve_state(RateMatrix, state, dt, rng=None):
     if rng is None:
         rng = np.random.default_rng()
 
@@ -123,17 +134,19 @@ def evolve_state(RateMatrix, state, dt, rng = None):
 
     return stateOut
 
+
 def convert_state_beta(state, S, stateVar=None):
     if stateVar is None:
         stateVar = generate_state_var(S)
 
-    methState = np.zeros((2*S+1, state.shape[1]))
+    methState = np.zeros((2 * S + 1, state.shape[1]))
     for index, (k, m) in enumerate(stateVar):
-        methState[k+2*m, :] += state[index, :]
+        methState[k + 2 * m, :] += state[index, :]
 
-    betaOut = np.linspace(0, 1, 2*S+1) @ methState
+    betaOut = np.linspace(0, 1, 2 * S + 1) @ methState
 
     return betaOut
+
 
 def beta_convert_params(mu, kappa):
     """
@@ -147,20 +160,23 @@ def beta_convert_params(mu, kappa):
         raise Exception("mu must be between 0 and 1")
 
     alpha = kappa * mu
-    beta = kappa * (1- mu)
+    beta = kappa * (1 - mu)
 
     return alpha, beta
+
 
 def rescale_beta(beta, delta, eta):
     # Linear transform of beta values from between
     # 0 and 1 to between delta and eta
     return (eta - delta) * beta + delta
 
+
 def add_noise(beta, delta, eta, kappa):
     beta_rescale = rescale_beta(beta, delta, eta)
     a, b = beta_convert_params(beta_rescale, kappa)
 
     return stats.beta.rvs(a, b)
+
 
 def all_parents(tree):
     parents = {}
@@ -169,13 +185,14 @@ def all_parents(tree):
             parents[child] = clade
     return parents
 
+
 def simulate_beta(tree, S, lam, mu, gamma, delta, eta, kappa, N):
     rng = np.random.default_rng()
 
     stateVar = generate_state_var(S)
     RateMatrix = generate_rate_matrix(S, lam, mu, gamma, stateVar)
 
-    initialConditions = np.zeros((int(0.5 * (S+1) * (S+2)), N))
+    initialConditions = np.zeros((int(0.5 * (S + 1) * (S + 2)), N))
     initialConditions[[0, -1], :] = 0.5
 
     stateInit = multinomial_rvs(1, initialConditions, rng)
@@ -184,13 +201,13 @@ def simulate_beta(tree, S, lam, mu, gamma, delta, eta, kappa, N):
     n = 0
     for clade in tree.find_clades():
         if clade.name is None:
-            clade.name = f'{n}'
+            clade.name = f"{n}"
             n += 1
 
     parent_dict = all_parents(tree)
 
     for clade in tree.find_clades():
-        if clade.name != '0':
+        if clade.name != "0":
             parent_clade = parent_dict[clade]
             clade.fcpgs = evolve_state(RateMatrix, parent_clade.fcpgs, clade.branch_length, rng)
             clade.beta = convert_state_beta(clade.fcpgs, S, stateVar)
@@ -198,34 +215,27 @@ def simulate_beta(tree, S, lam, mu, gamma, delta, eta, kappa, N):
 
     return tree
 
+
 def file_exists_callback(ctx, param, value):
     if value and not os.path.isfile(value):
         raise click.BadParameter(f"The provided tree file ({value}) does not exist")
     return value
 
-@click.command(cls=CustomCommand)
-@click.option("--nSites", "N", default=1000, help="Number of fCpGs to simulate", type=int, cls=CustomOption, help_group="Model parameters")
-@click.option("--nCells", "S", default=5, help="Number of stem cells to consider", type=int, cls=CustomOption, help_group="Model parameters")
-@click.option("--lam", default=1.3, help="Fixed lambda parameter value", type=float, cls=CustomOption, help_group="Model parameters")
-@click.option("--mu", default=0.03, help="Fixed mu parameter value", type=float, cls=CustomOption, help_group="Model parameters")
-@click.option("--gamma", default=0.03, help="Fixed gamma parameter value", type=float, cls=CustomOption, help_group="Model parameters")
-@click.option("--delta", default=0.05, help="Fixed delta parameter value", type=float, cls=CustomOption, help_group="Model parameters")
-@click.option("--eta", default=0.93, help="Fixed eta parameter value", type=float, cls=CustomOption, help_group="Model parameters")
-@click.option("--kappa", default=100, help="Fixed kappa parameter value", type=float, cls=CustomOption, help_group="Model parameters")
-@click.option("--random_tree", default=False, help="Use a random tree", is_flag=True, cls=CustomOption, help_group="Phylogenetic tree")
-@click.option("--tree_file", help="Tree file in Newick format to use", type=str, callback=file_exists_callback, cls=CustomOption, help_group="Phylogenetic tree")
-@click.option("--samples", default=10, help="Number of samples in the simulated tree", type=int, cls=CustomOption, help_group="Phylogenetic tree")
-@click.option("--eff_pop_size", default=10_000, help="Effective population size for the simulated tree", type=float, cls=CustomOption, help_group="Phylogenetic tree")
-@click.option("--time", default=30, help="Number of years for the simulated tree", type=int, cls=CustomOption, help_group="Phylogenetic tree")
-@click.option("--root_limits", default="5,25", help="Low and high limit the size of the root height. Comma-separated total years.", type=str, cls=CustomOption, help_group="Phylogenetic tree")
-@click.option("--seed", default=123, help="Set a seed", type=int, cls=CustomOption, help_group="Other")
-@click.option("--directory", default=".", help="Alternative root directory for the output", type=click.Path(exists=True), cls=CustomOption, help_group="Other")
-@click.option("--output", required = True, help="Prefix that output files will carry", type=str, cls=CustomOption, help_group="Other")
-@click.option("--no_tree", default=False, help="Whether to avoid plotting the phylogenetic tree", is_flag=True, cls=CustomOption, help_group="Other")
-def main(N, S, lam, mu, gamma, delta, eta, kappa, random_tree, tree_file, seed, samples, eff_pop_size, time, root_limits, directory, output, no_tree):
+
+def range_to_numeric_callback(ctx, param, value):
+    if value is not None:
+        if not re.match(r"^\d*\.?\d+,\s*\d*\.?\d+$", value):
+            raise click.BadParameter(f"The parameter should be formatted as 'min,max'. Passed input: {value}")
+        else:
+            nums = list(map(float, value.split(",")))
+            value = round(random.uniform(nums[0], nums[1]), 5)
+    return value
+
+
+def simulate_fcpgs(N, S, lam, mu, gamma, delta, eta, kappa, random_tree, tree_file, seed, samples, eff_pop_size, time, root_limits, directory, output, no_tree):
     np.random.seed(seed)
 
-    arguments = locals() # Get current variables as a dictionary
+    arguments = locals()  # Get current variables as a dictionary
 
     directory = f"{directory}/{output}"
     os.makedirs(directory, exist_ok=True)
@@ -252,19 +262,20 @@ def main(N, S, lam, mu, gamma, delta, eta, kappa, random_tree, tree_file, seed, 
     tree.rooted = True
 
     if not no_tree:
-        with PdfPages(f'{directory}/{output}Tree.pdf') as export_pdf:
+        with PdfPages(f"{directory}/{output}Tree.pdf") as export_pdf:
             fig, ax = plt.subplots()
             Phylo.draw(tree, axes=ax, do_show=False)
             plt.tick_params(
-                axis='y',          # changes apply to the x-axis
-                which='both',      # both major and minor ticks are affected
-                left=False,      # ticks along the bottom edge are off
-                right=False,         # ticks along the top edge are off
-                labelleft=False) # labels along the bottom edge are off
-            plt.ylabel('')
-            ax.spines['right'].set_visible(False)
-            ax.spines['top'].set_visible(False)
-            ax.spines['left'].set_visible(False)
+                axis="y",  # changes apply to the x-axis
+                which="both",  # both major and minor ticks are affected
+                left=False,  # ticks along the bottom edge are off
+                right=False,  # ticks along the top edge are off
+                labelleft=False,
+            )  # labels along the bottom edge are off
+            plt.ylabel("")
+            ax.spines["right"].set_visible(False)
+            ax.spines["top"].set_visible(False)
+            ax.spines["left"].set_visible(False)
             export_pdf.savefig()
             plt.close()
 
@@ -275,14 +286,14 @@ def main(N, S, lam, mu, gamma, delta, eta, kappa, random_tree, tree_file, seed, 
     for clade in tree.get_terminals():
         df[clade.name] = clade.betaNoisy
 
-    df.to_csv(f'{directory}/{output}Beta.csv')
+    df.to_csv(f"{directory}/{output}Beta.csv")
 
-    with PdfPages(f'{directory}/{output}SimulatedClustermap.pdf') as export_pdf:
+    with PdfPages(f"{directory}/{output}SimulatedClustermap.pdf") as export_pdf:
         sns.clustermap(df, cmap="vlag")
         export_pdf.savefig()
         plt.close()
 
-    with PdfPages(f'{directory}/{output}BetaHist.pdf') as export_pdf:
+    with PdfPages(f"{directory}/{output}BetaHist.pdf") as export_pdf:
         for col in df:
             fig, ax = plt.subplots()
             plt.hist(df[col], np.linspace(0, 1, 51), density=True, alpha=0.4)
@@ -293,8 +304,47 @@ def main(N, S, lam, mu, gamma, delta, eta, kappa, random_tree, tree_file, seed, 
             export_pdf.savefig()
             plt.close()
 
-    with open(f'{directory}/{output}Parameters.json', 'w') as fp:
+    with open(f"{directory}/{output}Parameters.json", "w") as fp:
         json.dump(arguments, fp, indent=1)
+
+
+@click.command(cls=CustomCommand)
+@click.option("--nSites", "-n", "N", default=1000, help="Number of fCpGs to simulate", type=int, cls=CustomOption, help_group="Model parameters")
+@click.option("--nCells", "-S", "S", default=5, help="Number of stem cells to consider", type=int, cls=CustomOption, help_group="Model parameters")
+@click.option("--lam", default=1.3, help="Fixed lambda parameter value", type=float, cls=CustomOption, help_group="Model parameters")
+@click.option("--mu", default=0.03, help="Fixed mu parameter value", type=float, cls=CustomOption, help_group="Model parameters")
+@click.option("--gamma", default=0.03, help="Fixed gamma parameter value", type=float, cls=CustomOption, help_group="Model parameters")
+@click.option("--delta", default=0.05, help="Fixed delta parameter value", type=float, cls=CustomOption, help_group="Model parameters")
+@click.option("--eta", default=0.93, help="Fixed eta parameter value", type=float, cls=CustomOption, help_group="Model parameters")
+@click.option("--kappa", default=100, help="Fixed kappa parameter value", type=float, cls=CustomOption, help_group="Model parameters")
+@click.option("--rlam", help="Randomly choose a fixed lambda parameter value (min,max)", type=str, callback=range_to_numeric_callback, cls=CustomOption, help_group="Random parameters")
+@click.option("--rmu", help="Randomly choose a fixed mu parameter value (min,max)", type=str, callback=range_to_numeric_callback, cls=CustomOption, help_group="Random parameters")
+@click.option("--metbias", help="Randomly choose a mu/gamma ratio (min,max)", type=str, callback=range_to_numeric_callback, cls=CustomOption, help_group="Random parameters")
+@click.option("--rdelta", help="Randomly choose a fixed delta parameter value (min,max)", type=str, callback=range_to_numeric_callback, cls=CustomOption, help_group="Random parameters")
+@click.option("--reta", help="Randomly choose a fixed eta parameter value (min,max)", type=str, callback=range_to_numeric_callback, cls=CustomOption, help_group="Random parameters")
+@click.option("--rkappa", help="Randomly choose a fixed kappa parameter value (min,max)", type=str, callback=range_to_numeric_callback, cls=CustomOption, help_group="Random parameters")
+@click.option("--random_tree", default=False, help="Use a random tree", is_flag=True, cls=CustomOption, help_group="Phylogenetic tree")
+@click.option("--tree_file", help="Tree file in Newick format to use", type=str, callback=file_exists_callback, cls=CustomOption, help_group="Phylogenetic tree")
+@click.option("--samples", default=10, help="Number of samples (leaves) in the simulated tree", type=int, cls=CustomOption, help_group="Phylogenetic tree")
+@click.option("--eff_pop_size", default=10_000, help="Effective population size for the simulated tree", type=float, cls=CustomOption, help_group="Phylogenetic tree")
+@click.option("--time", default=30, help="Number of years for the simulated tree", type=int, cls=CustomOption, help_group="Phylogenetic tree")
+@click.option("--root_limits", default="5,25", help="Low and high limit the size of the root height. Comma-separated total years.", type=str, cls=CustomOption, help_group="Phylogenetic tree")
+@click.option("--seed", help="Set a seed. A random seed will be set if none provided", type=int, cls=CustomOption, help_group="Other")
+@click.option("--directory", default=".", help="Alternative root directory for the output", type=click.Path(exists=True), cls=CustomOption, help_group="Other")
+@click.option("--output", required=False, help="Prefix that output files will carry", type=str, cls=CustomOption, help_group="Other")
+@click.option("--no_tree", default=False, help="Whether to avoid plotting the phylogenetic tree", is_flag=True, cls=CustomOption, help_group="Other")
+def main(N, S, lam, mu, gamma, delta, eta, kappa, rlam, rmu, metbias, rdelta, reta, rkappa, random_tree, tree_file, seed, samples, eff_pop_size, time, root_limits, directory, output, no_tree):
+    lam = rlam if rlam else lam
+    mu = rmu if rmu else mu
+    delta = rdelta if rdelta else delta
+    gamma = round(mu * metbias, 5) if metbias else gamma
+    kappa = rkappa if rkappa else kappa
+    eta = reta if reta else eta
+
+    output = output if output else f"{S}_{lam}_{mu}_{gamma}_{kappa}_{samples}"
+    seed = seed if seed else get_random_seed()
+    simulate_fcpgs(N, S, lam, mu, gamma, delta, eta, kappa, random_tree, tree_file, seed, samples, eff_pop_size, time, root_limits, directory, output, no_tree)
+
 
 if __name__ == "__main__":
     main()
